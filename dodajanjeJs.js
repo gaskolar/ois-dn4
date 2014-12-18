46e4bc5eb0e98fd086fe3efa8fd43833b731e4b5
Gasper Kolar <kolargas@gmail.com>
2014-12-18 11:55:35 +0100

    Dodana opcija za paciente int locena stran za hreiranje pacientov.

---
+++ dodajanjeJs.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#preberiPredlogoBolnika').change(function() {
+		$("#kreirajSporocilo").html("");
+		var podatki = $(this).val().split(",");
+		$("#kreirajIme").val(podatki[0]);
+		$("#kreirajPriimek").val(podatki[1]);
+		$("#kreirajDatumRojstva").val(podatki[2]);
+	});
+});
